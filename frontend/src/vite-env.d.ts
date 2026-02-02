19c065ade0e24bfa33bc8e9662667d4b4b7418ca
asifahemmed09 <asifahemmed.dev@gmail.com>
2026-02-03 01:06:13 +0600

    fix bug ( deployment)

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_CLERK_PUBLISHABLE_KEY: string
+  readonly MODE: string
+  // add more env variables as needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
